b0d6fbdf773fa24caeba5e1151ee3cdbd6cf41ca
skyler <pkphung2001@gmail.com>
2025-07-23 06:34:19 +0700

    Init

---
+++ player.gd
@@ -0,0 +1,38 @@
+extends Area2D
+@export var speed = 400
+var screen_size
+
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	
+func _process(delta):
+	var velocity = Vector2.ZERO
+	var input_map = {
+		"move_right": Vector2(1, 0),
+		"move_left": Vector2(-1, 0),
+		"move_down": Vector2(0, 1),
+		"move_up": Vector2(0, -1)
+	}
+	
+	for action in input_map:
+		if Input.is_action_pressed(action):
+			velocity += input_map[action]
+			
+			
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		# See the note below about the following boolean assignment.
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
