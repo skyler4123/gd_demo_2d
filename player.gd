12d70ae5b3f22aeb2f1f14812dd5666a1e38bc99
skyler <pkphung2001@gmail.com>
2025-07-23 07:31:05 +0700

    update

--- player.gd
+++ player.gd
@@ -1,9 +1,11 @@
 extends Area2D
+signal hit
 @export var speed = 400
 var screen_size
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	hide()
 	
 func _process(delta):
 	var velocity = Vector2.ZERO
@@ -36,3 +38,14 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _on_body_entered(body: Node2D) -> void:
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
